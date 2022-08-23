5507b779b192bd08527d5506da4a6978d948f377
Saxon Jensen <saxon.jensen@healthengine.com.au>
2022-08-23 22:17:48 +0800

    Figuring this out

--- src/Handler/Profile.hs
+++ src/Handler/Profile.hs
@@ -1,15 +1,43 @@
-{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+
 module Handler.Profile where
 
 import Import
 
 getProfileR :: Handler Html
 getProfileR = do
-    (_, user) <- requireAuthPair
-    defaultLayout $ do
-        setTitle . toHtml $ userIdent user <> "'s User page"
-        $(widgetFile "profile")
+  (_, user) <- requireAuthPair
+  (compWidget, compEncType) <- generateFormPost competitionCreationForm
+  profileLayout user compWidget compEncType Nothing
+
+postProfileR :: Handler Html
+postProfileR = do
+  (_, user) <- requireAuthPair
+  ((result, formWidget), formEncType) <- runFormPost competitionCreationForm
+  created <- case result of
+    FormSuccess c -> do
+      entity <- runDB $ insertEntity $ Competition (name c) (unTextarea $ description c) Nothing Nothing
+      return $ Just entity
+    _ -> return Nothing
+  profileLayout user formWidget formEncType created
+
+profileLayout user compWidget compEncType mCreated = defaultLayout $ do
+  setTitle . toHtml $ userIdent user <> "'s User page"
+  $(widgetFile "profile")
+
+data CompetitionForm = CompetitionForm
+  { name :: Text,
+    description :: Textarea
+  }
+
+competitionCreationForm :: Form CompetitionForm
+competitionCreationForm =
+  renderDivs $
+    CompetitionForm
+      <$> areq textField "Name" Nothing
+      <*> areq textareaField "Description" Nothing
