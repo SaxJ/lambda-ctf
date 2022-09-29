9d553ddbbf70ecd992040267dff4e6a661501057
Saxon Jensen <saxon.jensen@healthengine.com.au>
2022-09-29 14:11:48 +0800

    Fix config parsing

--- src/Settings.hs
+++ src/Settings.hs
@@ -94,6 +94,8 @@ instance FromJSON AppSettings where
         appCopyright              <- o .:  "copyright"
         appGoogleClientId              <- o .:  "googleid"
         appGoogleClientSecret              <- o .:  "googlesecret"
+        appSlackWebhook             <- o .:  "slackwebook"
+        appAllowedDomain             <- o .:  "allowedDomain"
         appAnalytics              <- o .:? "analytics"
 
         appAuthDummyLogin         <- o .:? "auth-dummy-login"      .!= dev
