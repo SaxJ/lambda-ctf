dc42ef319fbc3e9513df9db62204e01d145a8d51
Saxon Jensen <saxon.jensen@healthengine.com.au>
2023-02-25 15:09:07 +0800

    V2 Burn it all

--- Dockerfile.run
+++
@@ -1,1 +0,0 @@
-FROM ghcr.io/saxj/lambda-ctf:master
--- app/devel.hs
+++
@@ -1,6 +0,0 @@
-{-# LANGUAGE PackageImports #-}
-import "lambda-ctf" Application (develMain)
-import Prelude (IO)
-
-main :: IO ()
-main = develMain
